609625d7cc6742d0150455670ea1d75e221d89b6
Leos <highwind26@gmail.com>
2017-10-24 14:08:19 +0900

    로그인 수정
    변수 추가 수정

--- login.php
+++ login.php
@@ -1,3 +1,32 @@
+<?php #배열과 변수 설정
+    $iconarticle = ['img/icon/iconarticle01.png',
+                    'img/icon/iconarticle02.png',
+                    'img/icon/iconarticle03.png',
+                    'img/icon/iconarticle04.png'];
+
+    $article = ['좋아요',
+                '문구 더보기',
+                '댓글',
+                'Lorem, ipsum dolor sit amet consectetur adipisicing.',
+                '10월 1일'];
+
+    $link = ['INSTAGRAM 정보',
+            '지원',
+            '블로그',
+            '홍보 센터',
+            'API',
+            '채용',
+            '개인정보처리방침',
+            '약관',
+            '디렉터리',
+            '언어'];
+
+    $input = '<input class="comment" type="text" placeholder="댓글달기">';
+
+    $all = '<a href=""> 모두 보기</a>';
+
+    $count = ' #개';
+?>
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -24,24 +53,7 @@
         </div>
     </header>
     <article>
-        <?php #배열과 변수 설정
-        $iconarticle = ['img/icon/iconarticle01.png',
-                        'img/icon/iconarticle02.png',
-                        'img/icon/iconarticle03.png',
-                        'img/icon/iconarticle04.png'];
-
-        $article = ['좋아요',
-                    '문구 더보기',
-                    '댓글',
-                    'Lorem, ipsum dolor sit amet consectetur adipisicing.',
-                    '10월 1일'];
-
-        $input = '<input class="comment" type="text" placeholder="댓글달기">';
-
-        $all = '<a href="">모두 보기</a>';
-
-        $hr = '<hr>';
-        ?>
+        
         <?php for($i = 1; $i < 4; $i++){ #for 문 시작 ?>
 
          <div class="article">
@@ -56,13 +68,13 @@
                 <img src="<?= $iconarticle[2] ?>">
             </div>
             <div>
-                <p class="like"><?= $article[0] ?> #개</p>
+                <p class="like"><?= $article[0].$count ?> </p>
                 <p class="textmore"><?= $article[1] ?></p>
-                <p class="comment"><?= $article[2] ?> #개 <?= $all ?></p>
+                <p class="comment"><?= $article[2].$count ?><?= $all ?></p>
                 <p class="description"><?= $article[3] ?></p>
                 <p class="datetime"><?= $article[4] ?></p> 
             </div>
-            <?= $hr ?>
+            <hr>
             <p>
                 <?= $input ?>
                 <a href=""><img class="submit" src="<?= $iconarticle[3] ?>"></a>
@@ -72,18 +84,6 @@
     </article>
     <footer>
         <p>
-            <?php
-                $link = ['INSTAGRAM 정보',
-                        '지원',
-                        '블로그',
-                        '홍보 센터',
-                        'API',
-                        '채용',
-                        '개인정보처리방침',
-                        '약관',
-                        '디렉터리',
-                        '언어'];
-            ?>
             <?php for($i = 0; $i < 10; $i++) { #for 문 시작 ?>
                 <span class="footerlink" OnClick="location.href="" "><?= $link[$i] ?></span>
             <?php } #for 문 끝 ?>
