8be8d8e650569bdca2fc8fd22a63e936cb4ba7e5
Leos <highwind26@gmail.com>
2017-10-24 14:42:21 +0900

    Study exsample add file & code
    공부 내용 추가
    : PDO & Cookie & Session

---
+++ date.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>현제 시간은 <?php
+    echo date('Y-n-j H:m:s');
+    ?> 입니다.</h1>
+</body>
+</html>
+
