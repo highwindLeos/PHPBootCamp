9241ae3aec177d98ace74cfd3be7ef153f13397a
Leos <highwind26@gmail.com>
2017-10-30 19:50:31 +0900

    폼검증 업데이트
    폼검증 업데이트

--- js/validate.js
+++ js/validate.js
@@ -12,7 +12,7 @@ function checkForm() {
     var x = document.forms["fmField"]["email"].value;
     var atpos = x.indexOf("@");
     var dotpos = x.lastIndexOf(".");
-    if(atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length){
+    if(atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length){ //@ 가 한자 이상, . 이 한자 이하.
         window.alert("이메일 형식에 맞게 입력하시오");
         document.fmField.userEmail.focus();
         document.getElementById('userEmail').select();
@@ -39,6 +39,12 @@ function checkForm() {
     // 암호 입력 유무 체크
     if(document.fmField.userPassword.value == ''){
         alert('암호를 입력하세요.');
+        Password.focus();
+        return false; // 입력이 안되어 있다면 submint 이벤트를 중지
+    }
+    
+    if(document.fmField.userPassword.value < 6){
+        alert('암호를 6자 이상 입력하세요.');
         Password.focus();
         return false; // 입력이 안되어 있다면 submint 이벤트를 중지
     }
