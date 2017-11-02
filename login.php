ff8a71b128802227a941d8ff0899a04e00e389b1
Leos <highwind26@gmail.com>
2017-11-02 10:45:03 +0900

    View 분리 작업
    View 분리 작업

--- login.php
+++ login.php
@@ -4,9 +4,8 @@ $db = new PDO('mysql:host=localhost;dbname=anicoboard; charset=utf8', 'root', 's
     include './model/model.php'; #모델 클래스를 사용할 수 있게 포함시킨다.
 
 $Model = new Model($db);# 인스턴스를 만듭니다.
-
 $list = $Model->getAlls(); # 리스트를 가져옵니다.
 
 ?>
 
-<?php include './view/view.php'; # 뷰를 가져온다. ?>
+<?php include './view/loginView.php'; # 뷰를 가져온다. ?>
