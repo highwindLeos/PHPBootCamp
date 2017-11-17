5f28246c57997f4c69b4d155699bd13235260bd2
Leos <highwind26@gmail.com>
2017-11-18 06:02:10 +0900

    Write Process 수정
    Write Process  기능보완.

--- view/wirteView.php
+++ view/wirteView.php
@@ -13,7 +13,11 @@
                 <form class="article-write" enctype="multipart/form-data">
                     <label for="image_uploads"> 업로드 할 이미지 파일을 선택해주세요.(PNG, JPG)</label>
                     <input type="file" class="fileupload" name="image_uploads" accept=".jpg, .jpeg, .png" multiple>
+                    <p class="validate"><?php if(isset($_SESSION['picture'])) echo $_SESSION['picture'];
+                        unset($_SESSION['picture']) ?></p>
                     <textarea class="article-input" name="article" placeholder="내용을 입력하세요." ></textarea>
+                    <p class="validate"><?php if(isset($_SESSION['article'])) echo $_SESSION['article'];
+                        unset($_SESSION['article']) ?></p>
                     <button class="submit-btn" type="submit" formmethod="POST" formaction="writeProcess.php">
                         <img src="img/writebtn.png" alt="보내기">
                     </button>
--- writeProcess.php
+++ writeProcess.php
@@ -10,17 +10,41 @@ require 'config/config.php';
     {
         die($e->getMessage());
     }
-
+    
+    $article = filter_input(INPUT_POST, 'article', FILTER_DEFAULT); 
+    $picture = filter_var($_FILES['image_uploads']['name'], FILTER_DEFAULT); #FILE 배열을 필터.
+    
     $insertmodle = new InsertModel($db);
+    
+    if($_POST){ #POST에 값이 있다면 검증을 실행.
+        # 변수설정
+        $errors = array(); #에러 메세지를 담을 배열을 생성
+
+        # 검증 코드
+        if(empty($article))
+        {
+            $_SESSION['article'] = $errors['article'] = "* 내용은 빈칸일 수 없습니다.";
+        }
+        
+        if(empty($picture))
+        {
+            $_SESSION['picture'] = $errors['picture'] = "* 업로드 할 사진을 선택하지 않았습니다.";
+        }  
+    }
 
     if(!$_GET['name']){ # Cancle 버튼이 아니라면 True.
-        if($_FILES['image_uploads']['name']){ # 업로드 사진이 없다면.
+        if(count($errors) == 0 && $_FILES['image_uploads']['name']){ # 업로드 사진이 없다면.
             $insertmodle->UploadImageAndArticleId(); #사진을 업로드.
             $insertmodle->WriteArticles(); #내용을 업로드.
             header("Location: main.php");
         } else {
-            header("Location: main.php");
+            header("Location: write.php");
         }
     }
-
+    
+    if($_GET['name']){ # Cancle 버튼이 눌려서 값이 있다면 True.
+        $_SESSION['article'] = $_SESSION['picture'] = ''; #세션에 검증 메세지만 삭제 후 페이지 이동.
+        header("Location: main.php");
+    }
+    
 ?>
