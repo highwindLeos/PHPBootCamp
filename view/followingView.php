989013e38069f3218ae03190be62c93c735ea1d5
leos <highwind26@gmail.com>
2017-11-20 23:13:39 +0900

    CSS 수정 : profile
    CSS 수정 : profile

--- view/followingView.php
+++ view/followingView.php
@@ -3,27 +3,27 @@
     <article id="article">
        <div class="top-article">
             <a href="#open2"><img class="profile-img" src="<?= ($list['usericon']) ? $list['usericon'] : 'img/noimage.jpg'; ?>"> </a>
-            <p class="info">
-                <h2><?= $list['author']; ?></h2>
-                <a href="#"><img class="button" src="img/profile/profile2.png"></a>
-                <a href="#open1"><img class="setting" src="img/profile/profile3.png"></a>
-            </p>
-            <p class="count">
-                <span>게시물 <?= count($pitures); ?></span>
-                <span>팔로워 3</span>
-                <a href="#"><span>팔로우 6</span></a>
-            </p>
-            <p>
-                <?php if($list['author'] != $_SESSION['author']){ #현제 로그인된 사용자라면 버튼을 출력하지 않기. ?>
-                   <form class="follow">
-                        <input type="hidden" name="followUser" value="<?= $list['author']; ?>" />  
-                        <button class="follow-btn" formmethod="POST" name="unfollow" 
-                        value="<?= $_SESSION['author']; ?>" formaction="profile.php?author=<?= $list['author'] ?>">
-                        <?= $list['author']; ?> Following</button>
-                    </form>
-                <?php } ?>
-            </p>
-       </div>
+            <div class="info">
+                <div><h2><?= $list['author']; ?></h2></div>
+                <div><a href="#"><img class="button" src="img/profile/profile2.png"></a></div>
+                <div><a href="#open1"><img class="setting" src="img/profile/profile3.png"></a></div>
+            </div>
+            <div class="count">
+                <div><span>게시물 <?= count($pitures); ?></span></div>
+                <div><span>팔로워 3</span></div>
+                <div><a href="#"><span>팔로우 6</span></a></div>
+                <div>
+                    <?php if($list['author'] != $_SESSION['author']){ #현제 로그인된 사용자라면 버튼을 출력하지 않기. ?>
+                       <form class="follow">
+                            <input type="hidden" name="followUser" value="<?= $list['author']; ?>" />  
+                            <button class="follow-btn" formmethod="POST" name="unfollow" 
+                            value="<?= $_SESSION['author']; ?>" formaction="profile.php?author=<?= $list['author'] ?>">
+                            <?= $list['author']; ?> Following</button>
+                        </form>
+                    <?php } ?>
+                </div>
+            </div> 
+        </div>
         <div class="mid-article">
            <p><span><?= $list['author']; ?> 님의 게시물</span></p>
         </div>
